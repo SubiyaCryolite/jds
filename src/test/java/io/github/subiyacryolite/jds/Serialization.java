bbc728e31003d03bbfd9d2a9472dc13121ca9a6a
ifunga ndana <ifungandana@gmail.com>
2017-06-19 20:06:04 +0200

    Cleaning up repo

--- src/test/java/io/github/subiyacryolite/jds/Serialization.java
+++ src/test/java/io/github/subiyacryolite/jds/Serialization.java
@@ -26,15 +26,15 @@ public class Serialization extends BaseTestConfig {
 
     @Test
     public void testBlobSerialization() {
-        SimpleBlobProperty in = new SimpleBlobProperty(new byte[]{(byte)0xC9, (byte)0xCB, (byte)0xBB,
+        SimpleBlobProperty simpleBlobProperty = new SimpleBlobProperty(new byte[]{(byte)0xC9, (byte)0xCB, (byte)0xBB,
                 (byte)0xCC, (byte)0xCE, (byte)0xB9,
                 (byte)0xC8, (byte)0xCA, (byte)0xBC,
                 (byte)0xCC, (byte)0xCE, (byte)0xB9,
                 (byte)0xC9, (byte)0xCB, (byte)0xBB});
-        String canonicalName = in.getClass().getCanonicalName();
-        serialize(in, canonicalName);
-        SimpleBlobProperty out = deserialize(canonicalName, in.getClass());
-        System.out.printf("pre %s\n",Arrays.toString(in.get()));
+        String canonicalName = simpleBlobProperty.getClass().getCanonicalName();
+        serialize(simpleBlobProperty, canonicalName);
+        SimpleBlobProperty out = deserialize(canonicalName, simpleBlobProperty.getClass());
+        System.out.printf("pre %s\n",Arrays.toString(simpleBlobProperty.get()));
         System.out.printf("post %s\n",Arrays.toString(out.get()));
     }
 }
