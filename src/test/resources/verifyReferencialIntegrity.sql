ea0fcc6a769157ff7027065a8fb3ec4afdfdc30a
ifunga ndana <ifungandana@gmail.com>
2017-10-29 19:11:42 +0200

    CD: Updating older documentation and tests

--- src/test/resources/verifyReferencialIntegrity.sql
+++ src/test/resources/verifyReferencialIntegrity.sql
@@ -1,20 +1,20 @@
-DELETE FROM JdsStoreEntityOverview
-DELETE FROM JdsStoreEntityBinding
+DELETE FROM JdsEntityOverview
+DELETE FROM JdsEntityBinding
 
-insert into JdsStoreEntityOverview(EntityGuid) values('PatientA'),('PatientB'),('Address1_Id'),('Address2_Id'),('Address3_Id'), ('Address4_Id')
+insert into JdsEntityOverview(EntityGuid) values('PatientA'),('PatientB'),('Address1_Id'),('Address2_Id'),('Address3_Id'), ('Address4_Id')
 
-INSERT INTO JdsStoreEntityBinding(ParentEntityGuid,ChildEntityGuid,ChildEntityId) VALUES('PatientA', 'Address1_Id',  2),('PatientA', 'Address2_Id',  2),('PatientA', 'Address3_Id',  2),('PatientB', 'Address3_Id',  2),('PatientB', 'Address4_Id',  2)
+INSERT INTO JdsEntityBinding(ParentEntityGuid,ChildEntityGuid,ChildEntityId) VALUES('PatientA', 'Address1_Id',  2),('PatientA', 'Address2_Id',  2),('PatientA', 'Address3_Id',  2),('PatientB', 'Address3_Id',  2),('PatientB', 'Address4_Id',  2)
 
-SELECT * FROM JdsStoreEntityBinding
+SELECT * FROM JdsEntityBinding
 
-DELETE FROM JdsStoreEntityOverview WHERE EntityGuid = 'PatientB'
+DELETE FROM JdsEntityOverview WHERE EntityGuid = 'PatientB'
 
-SELECT * FROM JdsStoreEntityBinding
+SELECT * FROM JdsEntityBinding
 
-DELETE FROM JdsStoreEntityOverview WHERE EntityGuid = 'Address3_Id'
+DELETE FROM JdsEntityOverview WHERE EntityGuid = 'Address3_Id'
 
-SELECT * FROM JdsStoreEntityBinding
+SELECT * FROM JdsEntityBinding
 
-DELETE FROM JdsStoreEntityBinding WHERE ChildEntityGuid = 'Address1_Id'
+DELETE FROM JdsEntityBinding WHERE ChildEntityGuid = 'Address1_Id'
 
-SELECT * FROM JdsStoreEntityBinding
+SELECT * FROM JdsEntityBinding
