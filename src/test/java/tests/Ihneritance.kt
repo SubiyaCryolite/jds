132966329572e36a5adb35401eeb87e951d7c5b9
ifunga ndana <ifungandana@gmail.com>
2017-11-02 20:46:34 +0200

    Fixing build error

--- src/test/java/tests/Ihneritance.kt
+++ src/test/java/tests/Ihneritance.kt
@@ -4,11 +4,9 @@ import common.BaseTestConfig
 import entities.EntityA
 import entities.EntityB
 import entities.EntityC
-import io.github.subiyacryolite.jds.JdsEntity
 import io.github.subiyacryolite.jds.JdsLoad
 import io.github.subiyacryolite.jds.JdsSave
 import org.junit.jupiter.api.Test
-import java.util.*
 
 /**
  * Created by ifunga on 01/07/2017.
@@ -23,12 +21,12 @@ class Ihneritance : BaseTestConfig() {
 
     @Throws(Exception::class)
     override fun load() {
-        val entityAs = JdsLoad.load(jdsDb, EntityA::class.java)
-        val entityBs = JdsLoad.load(jdsDb, EntityB::class.java)
-        val entityCs = JdsLoad.load(jdsDb, EntityC::class.java)
-        System.out.printf("All A s [%s]\n", entityAs)
-        System.out.printf("All B s [%s]\n", entityBs)
-        System.out.printf("All C s [%s]\n", entityCs)
+        val entityAs = JdsLoad(jdsDb, EntityA::class.java)
+        val entityBs = JdsLoad(jdsDb, EntityB::class.java)
+        val entityCs = JdsLoad(jdsDb, EntityC::class.java)
+        System.out.printf("All A s [%s]\n", entityAs.call())
+        System.out.printf("All B s [%s]\n", entityBs.call())
+        System.out.printf("All C s [%s]\n", entityCs.call())
     }
 
     @Test
