3f2b5f5c79b76cc8b1600eb650f5fdf769e26222
ifunga ndana <ifungandana@gmail.com>
2019-02-18 20:50:20 +0200

    Purged module-info class

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-//module io.github.subiyacryolite.jds {
-//    requires kotlin.stdlib;
-//    requires kotlin.stdlib.jdk7;
-//    requires java.base;
-//    requires java.sql;
-//    requires com.fasterxml.jackson.annotation;
-//    requires javafx.base;
-//}
