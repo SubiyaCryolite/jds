4575da59601ad025fd71d209c631068d698b556c
ifunga ndana <ifungandana@gmail.com>
2019-02-18 20:41:20 +0200

    Updating deep clone function

--- src/main/kotlin/io/github/subiyacryolite/jds/utility/DeepCopy.kt
+++ src/main/kotlin/io/github/subiyacryolite/jds/utility/DeepCopy.kt
@@ -9,22 +9,18 @@ object DeepCopy {
      *
      */
     @Throws(IOException::class, ClassNotFoundException::class)
-    fun <T : Any> clone(source: T): T? {
-        try {
-            ByteArrayOutputStream().use { byteArrayOutputStream ->
-                ObjectOutputStream(byteArrayOutputStream).use { objectOutputStream ->
-                    objectOutputStream.writeObject(source)
-                    ByteArrayInputStream(byteArrayOutputStream.toByteArray()).use { byteArrayInputStream ->
-                        ObjectInputStream(byteArrayInputStream).use { objectInputStream ->
-                            return objectInputStream.readObject() as T
-                        }
-                    }
+    fun <T : Serializable> clone(source: T?): T? {
+        if (source == null) return null
+        ByteArrayOutputStream().use { byteArrayOutputStream ->
+            ObjectOutputStream(byteArrayOutputStream).use { objectOutputStream ->
+                objectOutputStream.writeObject(source)
+            }
+            ByteArrayInputStream(byteArrayOutputStream.toByteArray()).use { byteArrayInputStream ->
+                ObjectInputStream(byteArrayInputStream).use { objectInputStream ->
+                    @Suppress("unchecked_cast")
+                    return objectInputStream.readObject() as T
                 }
             }
-        } catch (e: IOException) {
-            return null
-        } catch (e: ClassNotFoundException) {
-            return null
         }
     }
 }
